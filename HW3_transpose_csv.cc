7ccee7eaf964a11d3910c917ef7633d63dfa8ad2
Brian Phan <40909481+bachsofttrick@users.noreply.github.com>
2026-02-23 11:05:38 -0800

    Create HW3_transpose_csv.cc

---
+++ HW3_transpose_csv.cc
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <time.h>
+#include <thread>
+#include <algorithm>
+#define MAXN 1024
+#define T 10000
+
+using namespace std;
+
+double input[MAXN][MAXN];
+double output[MAXN][MAXN];
+double inputB[MAXN][MAXN];
+double outputB[MAXN][MAXN];
+
+double get_seconds() {
+  time_t res = time(NULL);
+  return (double) res;
+}
+
+void non_blocking(double output[MAXN][MAXN], double input[MAXN][MAXN], int n, double* result) {
+  double time0 = get_seconds();
+
+  for(int trials=0;trials<T;trials++) {
+    for(int i=0;i<n;i++) {
+      for(int j=0;j<n;j++) {
+        output[j][i]=input[i][j];
+      }
+    }
+  }
+
+  *result = get_seconds() - time0;
+}
+
+void blocking(double output[MAXN][MAXN], double input[MAXN][MAXN], int n, int b, double* result) {
+  double time0 = get_seconds();
+
+  for(int trials=0;trials<T;trials++) {
+    for(int m=0;m<n;m+=b) {
+      for(int k=0;k<n;k+=b) {
+        for(int i=m;i<min(m+b, n);i++) {
+          for(int j=k;j<min(k+b, n);j++) {
+            output[j][i]=input[i][j];
+          }
+        }
+      }
+    }
+  }
+
+  *result = get_seconds() - time0;
+}
+
+int main(int argc, char* argv[]) {
+  int N_values[] = {256, 512, 1024};
+  int B_values[] = {8, 16, 32, 64};
+
+  FILE* fp = fopen("results.csv", "w");
+  fprintf(fp, "N;B;non-blocking (s);blocking (s)\n");
+
+  for(int ni=0;ni<sizeof(N_values)/sizeof(N_values[0]);ni++) {
+    int n = N_values[ni];
+
+    for(int bi=0;bi<sizeof(B_values)/sizeof(B_values[0]);bi++) {
+      int b = B_values[bi];
+
+      for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+          input[i][j]=i;
+          inputB[i][j]=i;
+          output[i][j]=0.0;
+          outputB[i][j]=0.0;
+        }
+      }
+
+      double t_nb, t_b;
+      thread thread1(non_blocking, output, input, n, &t_nb);
+      thread thread2(blocking, outputB, inputB, n, b, &t_b);
+
+      thread1.join();
+      thread2.join();
+
+      fprintf(fp, "%d;%d;%lf;%lf\n", n, b, t_nb, t_b);
+    }
+  }
+
+  fclose(fp);
+  return 0;
+}
